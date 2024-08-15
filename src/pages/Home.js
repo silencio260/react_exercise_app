c169d9b1692629bbcf6560930e0e22d36680a780
silencio260 <faruq5840@gmail.com>
2024-08-15 19:47:05 +0100

    Implement Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import { Box } from '@mui/material'
+import React, { useState } from 'react'
+import HeroBanner from '../components/HeroBanner'
+import SearchExcercises from '../components/SearchExcercises'
+import Exercises from '../components/Exercises'
 
 const Home = () => {
+  const [exercises, setExercises] = useState([])
+  const [bodyPart, setBodyPart] = useState(['all'])
+
   return (
-    <div>Home</div>
+    <Box>
+      <HeroBanner />
+      <SearchExcercises 
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises            
+        bodyPart={bodyPart}
+        exercises={exercises}
+        setExercises={setExercises}
+      />
+    </Box>
   )
 }
 
